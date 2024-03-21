a5906b77b0f57c0b71e93a0cd681701f0170ecd6
setadrift <40219490+setadrift@users.noreply.github.com>
2024-03-21 14:31:31 +0000

    plot metrics update

--- streamlit-ml/app.py
+++ streamlit-ml/app.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
 
 def main():
     st.title("Binary Classification Web App")
@@ -33,17 +33,17 @@ def main():
     def plot_metrics(metrics_list):
         if 'Confusion Matrix' in metrics_list:
             st.subheader("Confusion Matrix")
-            plot_confusion_matrix(model, x_test, y_test, display_labels=class_names)
+            ConfusionMatrixDisplay.from_estimator(model, x_test, y_test, display_labels=class_names)
             st.pyplot()
 
         if 'ROC Curve' in metrics_list:
             st.subheader("ROC Curve")
-            plot_roc_curve(model, x_test, y_test)
+            RocCurveDisplay.from_estimator(model, x_test, y_test)
             st.pyplot()
-        
+
         if 'Precision-Recall Curve' in metrics_list:
             st.subheader('Precision-Recall Curve')
-            plot_precision_recall_curve(model, x_test, y_test)
+            PrecisionRecallDisplay.from_estimator(model, x_test, y_test)
             st.pyplot()
 
     df = load_data()
